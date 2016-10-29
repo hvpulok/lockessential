0a4244092d587208226a6526426911c14b6cc981
hvpulok <hvpulok@gmail.com>
2016-10-29 19:10:28 -0400

    Chore: Refactor code to rename view1 to information-manager

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'myApp.informationManager',
   'myApp.view2',
   'myApp.version',
   'navbar'
@@ -11,5 +11,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/information_manager'});
 }]);
