9a7a10011c7a9bbc5fc4b63c7993312dd086b591
hvpulok <hvpulok@gmail.com>
2016-11-10 23:30:38 -0500

    Add Custom directive which will make any text inline editable

--- app/shared/information-data/add-information.directive.js
+++ app/shared/information-data/add-information.directive.js
@@ -6,4 +6,30 @@ angular.module('information.data')
             scope: { inputData: '=newData'},
             templateUrl: 'shared/information-data/add-information.html'
         };
-    });
+    })
+    .directive('editable', function(){
+        return {
+            restrict: 'EA',
+            transclude: true,
+            templateUrl: 'shared/information-data/editable.html',
+            require: '?^parentCtrl',
+            scope: {'data': '=',
+                    'item': '='    
+            },
+            controller: function($scope, $element, $attrs){
+                $scope.isShown = false;
+                $scope.setIsShown = function(state){
+                    $scope.isShown = state;
+                }
+                $scope.save= function(){
+                    $scope.isShown = false;
+                    $scope.data[$scope.item] = $element.text();
+                    console.log($scope.data);
+                }
+                
+        },
+            link: function(scope, elm, attr, controller){
+                elm.attr('contenteditable','true');
+            }
+        };
+    })
