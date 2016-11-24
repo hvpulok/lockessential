9b6029acc2d1e2fac97b47af9d83344560e71b0b
hvpulok <hvpulok@gmail.com>
2016-11-24 13:43:52 -0500

    Feat: Default user role update as admin

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -81,7 +81,7 @@ var UserSchema = new Schema({
       type: String,
       enum: ['user', 'admin']
     }],
-    default: ['user'],
+    default: ['admin'],
     required: 'Please provide at least one role'
   },
   updated: {
