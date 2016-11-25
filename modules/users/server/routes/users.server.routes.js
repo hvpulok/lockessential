5efe143fb416a006f35f073493bde54b6b726890
hvpulok <hvpulok@gmail.com>
2016-11-24 20:28:46 -0500

    Server: Write an api to get current user owned accounts

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -3,6 +3,7 @@
 module.exports = function (app) {
   // User Routes
   var users = require('../controllers/users.server.controller');
+  var User = require('../models/user.server.model');
 
   // Setting up the users profile api
   app.route('/api/users/me').get(users.me);
@@ -10,6 +11,20 @@ module.exports = function (app) {
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
+  //my custom routes
+
+  //api route to get all current users owned accounts
+  app.route('/api/users/:id/accounts').get(function(req, res){
+    User.findById(req.user._id).populate("accounts").exec(function(err, foundUser){
+      if(err){
+        console.log(err);
+      }else {
+        console.log("found user");
+        console.log(foundUser);
+        res.jsonp(foundUser);
+      }
+    });
+  });
 
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
