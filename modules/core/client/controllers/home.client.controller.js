1df1f18e3b288819ff5cc3b37dd9ca084a96c7e4
hvpulok <hvpulok@gmail.com>
2016-11-25 17:42:22 -0500

    Feat: UI user Logged in checker

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.isLoggedIn = function(){
+      if(Authentication.user._id)
+        return true;
+      else
+        return false;
+    };
   }
 ]);
