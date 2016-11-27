917eec9f50e0fff796a6b3406595a509a3c1d41e
hvpulok <hvpulok@gmail.com>
2016-11-27 12:49:24 -0500

    Feat: Redsign structure of account create from

--- modules/accounts/client/controllers/accounts.client.controller.js
+++ modules/accounts/client/controllers/accounts.client.controller.js
@@ -10,28 +10,69 @@
 
   function AccountsController ($scope, $state, $window, Authentication, account) {
     var vm = this;
-
     vm.authentication = Authentication;
     vm.account = account;
     vm.error = null;
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-    vm.cards = [1];
     vm.addCard = function(){
-      vm.cards.push(vm.cards.length+1);
+      vm.account.card.push({
+          cardNumber: '',
+          cardExpDate: '',
+          nameOnCard: '',
+          securityCode: '',
+          cardType: '',
+          creditLimit: ''
+        });
     };
     vm.removeCard = function(index){
-
-      if(vm.cards.length>1){
+      if(vm.account.card.length>1){
         if(index){
-          vm.cards.splice(index-1, 1);
+          vm.account.card.splice(index-1, 1);
         }else{
-          vm.cards.pop();
+          vm.account.card.pop();
         }
       }
     };
-
+    // seeding dB ============Should be removed after dev mode==========
+    var seed = {
+      web: {
+        account: 'Amazon',
+        username: 'hvpulok',
+        password: 1234,
+        email: 'hvpulok@gmail.com',
+        url: 'www.amazon.com',
+        description: 'My Amazon Account'
+      },
+      bank: {
+        bankName: 'Ally',
+        acNumber: 123478902654,
+        bankRoutingNumber: 87823468,
+        fullName: 'Md Kamrul Hasan Pulok'
+      },
+      card: [{
+          cardNumber: 123456781234,
+          cardExpDate: '08/21',
+          nameOnCard: 'Kamrul H Pulok',
+          securityCode: 123,
+          cardType: 'Mastercard',
+          creditLimit: 5000
+        },
+        {
+          cardNumber: 555555555555,
+          cardExpDate: '08/21',
+          nameOnCard: 'Kamrul H Pulok',
+          securityCode: 123,
+          cardType: 'Mastercard',
+          creditLimit: 5000
+        }
+        ],
+      miscs: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    };
+    Object.assign(vm.account, seed);
+    console.log(vm.account);
+    // seeding dB ============Should be removed after dev mode============
     // Remove existing Account
     function remove() {
       if ($window.confirm('Are you sure you want to delete?')) {
@@ -48,9 +89,10 @@
 
       // TODO: move create/update logic to service
       if (vm.account._id) {
-        vm.account.$update(successCallback, errorCallback);
+        // vm.account.$update(successCallback, errorCallback);
       } else {
-        vm.account.$save(successCallback, errorCallback);
+        // vm.account.$save(successCallback, errorCallback);
+        console.log(vm.account);
       }
 
       function successCallback(res) {
