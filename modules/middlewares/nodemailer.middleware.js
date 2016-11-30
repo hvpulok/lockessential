48606df3d3a7313802d786f00b92bc6c9f0f89ad
hvpulok <hvpulok@gmail.com>
2016-11-29 20:23:24 -0500

    Chore

---
+++ modules/middlewares/nodemailer.middleware.js
@@ -0,0 +1,25 @@
+'use strict';
+var nodemailer = require('nodemailer');
+var mailKey = process.env.mailKey || '';
+var mailAddr = process.env.mailAddr || '';
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://mailAddr:mailKey@smtp.gmail.com');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+  from: '"My Manager App ?" <hvpulok@gmail.com>', // sender address
+  to: 'hvpulok@gmail.com, hvpulok@hotmail.com', // list of receivers
+  subject: 'Hello From MyMan ✔', // Subject line
+  text: 'Hello world ?', // plaintext body
+  html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+exports.sendEmail = function () {
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+};
