62a00f65e3fe30467a9c69ce727ddece9ac06f82
hvpulok <hvpulok@gmail.com>
2016-12-18 12:53:52 -0500

    Feat: Update userkey minlength and also visibility logic

--- modules/accounts/client/controllers/userKey.client.controller.js
+++ modules/accounts/client/controllers/userKey.client.controller.js
@@ -9,14 +9,22 @@
 
   function UserKeyController(AccountsService, $state, CryptoService) {
     var vm = this;
+    vm.isKeyShow = true; // flag to show user entered userkey while typing or not
+
     vm.userKey= CryptoService.getUserKey();
     if(!vm.userKey){
       $state.go('accounts.userKey');
     }
+
     vm.setKey = function(input){
       vm.userKey = input;
       CryptoService.setUserKey(input);
       $state.go('accounts.list');
     };
+
+    vm.toggleisKeyShow = function(){
+      vm.isKeyShow = !vm.isKeyShow;
+    }
+
   }
 }());
