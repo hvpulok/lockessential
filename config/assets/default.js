0bb4f14090364f39581788597b5d96c6bc98117b
hvpulok <hvpulok@gmail.com>
2017-01-13 22:51:21 -0500

    remove server socket asset config

--- config/assets/default.js
+++ config/assets/default.js
@@ -60,7 +60,7 @@ module.exports = {
     allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js', 'modules/middlewares/**/*.js'],
     models: 'modules/*/server/models/**/*.js',
     routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
-    sockets: 'modules/*/server/sockets/**/*.js',
+    // sockets: 'modules/*/server/sockets/**/*.js',
     config: ['modules/*/server/config/*.js'],
     policies: 'modules/*/server/policies/*.js',
     views: ['modules/*/server/views/*.html']
