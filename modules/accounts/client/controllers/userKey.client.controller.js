4af29def1e6b84337c4f265ff0983ad4140d1cda
hvpulok <hvpulok@gmail.com>
2017-01-31 20:40:43 -0500

    Chore: beautify the code

--- modules/accounts/client/controllers/userKey.client.controller.js
+++ modules/accounts/client/controllers/userKey.client.controller.js
@@ -5,31 +5,30 @@
     .module('accounts')
     .controller('UserKeyController', UserKeyController);
 
-  UserKeyController.$inject = ['AccountsService', '$state', 'CryptoService' , '$window'];
+  UserKeyController.$inject = ['AccountsService', '$state', 'CryptoService', '$window'];
 
   function UserKeyController(AccountsService, $state, CryptoService, $window) {
     var vm = this;
     vm.isKeyShow = true; // flag to show user entered userkey while typing or not
-    vm.gotoTop = function(){
-      $window.scroll(0,0);
+    vm.gotoTop = function () {
+      $window.scroll(0, 0);
     };
     vm.gotoTop(); // on page load the scroll should be on top of page
 
-    vm.userKey= CryptoService.getUserKey();
-    vm.setKey = function(input){
+    vm.userKey = CryptoService.getUserKey();
+    vm.setKey = function (input) {
       vm.userKey = input;
       CryptoService.setUserKey(input);
-      if($state.params.accountId){
-        $state.go('accounts.view', {accountId : $state.params.accountId});
+      if ($state.params.accountId) {
+        $state.go('accounts.view', { accountId: $state.params.accountId });
       }
-      else{
+      else {
         $state.go('accounts.list');
       }
     };
 
-    vm.toggleisKeyShow = function(){
+    vm.toggleisKeyShow = function () {
       vm.isKeyShow = !vm.isKeyShow;
     };
-
   }
-}());
+} ());
