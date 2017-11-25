94134c3d629daca381ab03ce363995043303ea96
hvpulok <hvpulok@gmail.com>
2017-11-25 10:03:19 -0700

    add dbbackup script

--- mongodb-backup/backup.js
+++ mongodb-backup/backup.js
@@ -1,19 +1,22 @@
 'use strict';
 var backup = require('mongodb-backup');
+try {
+    var env = require('./local-production');
+} catch (error) {
+}
 
 /*
  * https://www.npmjs.com/package/mongodb-backup
  */
 backup({
-    uri: 'mongodb://dumper:ILoveDenver65@ds159767.mlab.com:59767/my_manager', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
+    uri: process.env.mongodbURI || '', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
     root: __dirname, // write files into this dir
-    tar: 'dump.tar',
+    tar: 'dump_' + new Date().toDateString() + '_.tar',
     callback: function (err) {
         if (err) {
             console.error(err);
         } else {
-            console.log('finish');
+            console.log('Backup finished');
         }
     }
-    // collections: ['accounts', 'articles', 'articles',], // save this collection only
 });
