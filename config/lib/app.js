d4a6ba470f951b9d6b1f072886119a9a8e366651
hvpulok <hvpulok@gmail.com>
2017-11-25 00:51:27 -0700

    fix ssl issue

--- config/lib/app.js
+++ config/lib/app.js
@@ -7,7 +7,9 @@ var config = require('../config'),
   mongoose = require('./mongoose'),
   express = require('./express'),
   chalk = require('chalk'),
-  seed = require('./seed');
+  seed = require('./seed'),
+  fs = require('fs');
+var https = require('https');
 
 function seedDB() {
   if (config.seedDB && config.seedDB.seed) {
@@ -32,26 +34,40 @@ module.exports.start = function start(callback) {
   var _this = this;
 
   _this.init(function (app, db, config) {
-
     // Start the app by listening on <port> at <host>
     app.listen(config.port, config.host, function () {
-      // Create server URL
-      var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
-      // Logging initialization
-      console.log('--');
-      console.log(chalk.green(config.app.title));
-      console.log();
-      console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
-      console.log(chalk.green('Server:          ' + server));
-      console.log(chalk.green('Database:        ' + config.db.uri));
-      console.log(chalk.green('App version:     ' + config.meanjs.version));
-      if (config.meanjs['meanjs-version'])
-        console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
-      console.log('--');
-
-      if (callback) callback(app, db, config);
+      showMsg(config.port, config.host, callback);
     });
+
+    if (process.env.NODE_ENV === 'secure') {
+      var privateKey = fs.readFileSync(config.secure.privateKey);
+      var certificate = fs.readFileSync(config.secure.certificate);
+      https.createServer({
+        key: privateKey,
+        cert: certificate
+      }, app).listen(config.securePort, config.host, function () {
+        showMsg(config.securePort, config.host, callback);
+      });
+    }
 
   });
 
 };
+
+function showMsg(port, host, callback) {
+  // Create server URL
+  var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + host + ':' + port;
+  // Logging initialization
+  console.log('--');
+  console.log(chalk.green(config.app.title));
+  console.log();
+  console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
+  console.log(chalk.green('Server:          ' + server));
+  console.log(chalk.green('Database:        ' + config.db.uri));
+  console.log(chalk.green('App version:     ' + config.meanjs.version));
+  if (config.meanjs['meanjs-version'])
+    console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
+  console.log('--');
+
+  // if (callback) callback(app, db, config);
+}
