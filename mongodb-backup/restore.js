fb9896762c275951aa60ed1b313430daa633df37
hvpulok <hvpulok@gmail.com>
2018-01-11 21:56:33 -0700

    Update restore uri

--- mongodb-backup/restore.js
+++ mongodb-backup/restore.js
@@ -7,11 +7,7 @@ var restore = require('mongodb-restore');
  */
 
 restore({
-<<<<<<< HEAD
     uri: 'mongodb://localhost/my_manager', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
-=======
-    uri: 'mongodb://localhost:27017/mean-dev', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
->>>>>>> 38fbe542a661ff295e3e9888d7f2ed836b607c5f
     root: __dirname,
     tar: 'dump.tar',
     callback: function (err) {
