3ead2fd022b6fce6f731d4572131eaf599266887
hvpulok <hvpulok@gmail.com>
2018-07-01 23:08:39 -0600

    Add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'lockessential',
+      script: 'server.js',
+      exec_mode: 'cluster',
+      instances: '4',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/development',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
